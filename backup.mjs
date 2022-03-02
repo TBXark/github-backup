451d40c42dd59428afd47af7052d5f9e9f1d5e49
TBXark <tbxark@outlook.com>
2022-03-02 14:39:03 +0800

    replace github api

--- backup.mjs
+++ backup.mjs
@@ -2,106 +2,125 @@
 
 //import "zx/globals";
 
-await $`git --version`
-await $`pwd`
+await $`git --version`;
+await $`pwd`;
 
 async function loadConfig(path) {
   let content = {};
   if (fs.pathExistsSync(path)) {
     content = await fs.readJson(path);
-  } 
+  }
   if (!content.username) {
     content.username = await question("Please enter your username: ");
   }
   if (!content.token) {
     content.token = await question("Please enter your token: ");
   }
-  if (!content.repos)  {
-    content.repos = {}
+  if (!content.repos) {
+    content.repos = {};
   }
   return content;
 }
 
 async function fetchRepos(username, token) {
-  let response = await fetch(`https://api.github.com/users/${username}/repos`, {
-    method: "GET",
-    headers: { Authorization: `token ${token}` },
-  });
-  response = await response.json();
-  response = response.map((ele) => {
-    return {
-      name: ele.name,
-      status: {
-        private: ele.private,
-        fork: ele.fork,
-        archived: ele.archived,
-      },
-      date: {
-        created_at: ele.created_at,
-        updated_at: ele.updated_at,
-      },
-      ssh_url: ele.ssh_url,
-    };
-  });
   let store = {};
-  for (const repo of response) {
-    store[repo.name] = repo;
+  let page = 0;
+
+  while (true) {
+    let response = await fetch(
+      `https://api.github.com/search/repositories?q=user%3Atbxark&page=${page}`,
+      {
+        method: "GET",
+        headers: { Authorization: `token ${token}` },
+      }
+    );
+    response = await response.json();
+    const total = response.total_count
+    response = response.items.map((ele) => {
+      return {
+        name: ele.name,
+        status: {
+          private: ele.private,
+          fork: ele.fork,
+          archived: ele.archived,
+        },
+        date: {
+          created_at: ele.created_at,
+          updated_at: ele.updated_at,
+        },
+        ssh_url: ele.ssh_url,
+      };
+    });
+    if (response.length == 0) {
+      break;
+    }
+    for (const repo of response) {
+      store[repo.name] = repo;
+    }
+    page += 1;
+    if (Object.keys(store).length >= total) {
+      break;
+    }
   }
+
   return store;
 }
 
 function updateRepos(config, remoteRepos) {
-  const localReposKeys = Object.keys(config.repos)
-  config.repos = remoteRepos
-  return Object.keys(remoteRepos).filter(r => !localReposKeys.includes(r.name))
+  const localReposKeys = Object.keys(config.repos);
+  config.repos = remoteRepos;
+  return Object.keys(remoteRepos).filter(
+    (r) => !localReposKeys.includes(r.name)
+  );
 }
 
 function configPath() {
-  let path = process.argv.filter(x => x.startsWith('--path=')).map(x => x.replace('--path=', ''))
+  let path = process.argv
+    .filter((x) => x.startsWith("--path="))
+    .map((x) => x.replace("--path=", ""));
   if (path.length == 0) {
-    return './.github_backup_config.json'
+    return "./.github_backup_config.json";
   } else {
-    return path[0]
+    return path[0];
   }
 }
 
-
-let cPath = configPath()
+let cPath = configPath();
 let config = await loadConfig(cPath);
-const cloneAll = process.argv.indexOf('--clone=all') >= 0
-const remoteRepos = await fetchRepos(config.username, config.token)
+const cloneAll = process.argv.indexOf("--clone=all") >= 0;
+const remoteRepos = await fetchRepos(config.username, config.token);
+
+// console.log(Object.keys(remoteRepos).join('\n'))
 
 for (const name of updateRepos(config, remoteRepos)) {
-  const path = `./${name}`
+  const path = `./${name}`;
   if (fs.pathExistsSync(path)) {
     const del = await question(`Delete ${path}? (y/n): `, {
-      choices: ['y', 'n']
-    })
-    if (del === 'y') {
-      await fs.remove(path)
+      choices: ["y", "n"],
+    });
+    if (del === "y") {
+      await fs.remove(path);
     }
   }
 }
 
 for (const name of Object.keys(config.repos)) {
-  const path = `./${name}`
-  const repo = remoteRepos[name]
+  const path = `./${name}`;
+  const repo = remoteRepos[name];
   if (fs.pathExistsSync(path)) {
-    await $`cd ${path} && git pull`
+    await $`cd ${path} && git pull`;
   } else {
     if (cloneAll) {
-      await $`git clone ${repo.ssh_url}`
-      continue
+      await $`git clone ${repo.ssh_url}`;
+      continue;
     }
     const clone = await question(`Clone ${repo.ssh_url}? (y/n): `, {
-      choices: ['y', 'n']
-    })
-    if (clone === 'y') {
-      await $`git clone ${repo.ssh_url}`
+      choices: ["y", "n"],
+    });
+    if (clone === "y") {
+      await $`git clone ${repo.ssh_url}`;
     }
   }
 }
 
-
-
-await fs.writeFile(cPath, JSON.stringify(config, null, 2))
+await fs.writeFile(cPath, JSON.stringify(config, null, 2));
