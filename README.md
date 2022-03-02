fb9d1fa08366a67e6e6b9ecdaafe4be65ef5231a
TBXark <tbxark@outlook.com>
2022-03-02 11:15:21 +0800

    init project

---
+++ README.md
@@ -0,0 +1,31 @@
+# Github backup
+
+backup all github repo to local
+
+### Install
+
+
+```shell
+
+npm i -g zx
+wget https://raw.githubusercontent.com/tbxark/github-backup/master/backup.mjs
+
+```
+
+### Usage
+
+```
+./backup.mjs
+
+#or
+
+./backup.mjs --path=./you_config_path.json
+```
+
+
+
+#### 碎碎念
+
+最近看得到v2ex上有人github账户被封，俄罗斯也被米国盟友各种制裁。感觉把所有代码放在Github上不备份其实不太安全。
+现有其他git平台不能实时同步github的仓库。得在每个github的repo添加action去同步，过于麻烦。
+所以还是先备份到本地，然后再同步到另外的git平台。而且本地多一个备份也比较安全。
---
+++ backup.mjs
@@ -0,0 +1,96 @@
+#!/usr/bin/env zx
+
+import "zx/globals";
+
+
+async function loadConfig(path) {
+  let content = {};
+  if (fs.pathExistsSync(path)) {
+    content = await fs.readJson(path);
+  } 
+  if (!content.username) {
+    content.username = await question("Please enter your username: ");
+  }
+  if (!content.token) {
+    content.token = await question("Please enter your token: ");
+  }
+  if (!content.repos)  {
+    content.repos = {}
+  }
+  return content;
+}
+
+async function fetchRepos(username, token) {
+  let response = await fetch(`https://api.github.com/users/${username}/repos`, {
+    method: "GET",
+    headers: { Authorization: `token ${token}` },
+  });
+  response = await response.json();
+  response = response.map((ele) => {
+    return {
+      name: ele.name,
+      status: {
+        private: ele.private,
+        fork: ele.fork,
+        archived: ele.archived,
+      },
+      date: {
+        created_at: ele.created_at,
+        updated_at: ele.updated_at,
+      },
+      ssh_url: ele.ssh_url,
+    };
+  });
+  let store = {};
+  for (const repo of response) {
+    store[repo.name] = repo;
+  }
+  return store;
+}
+
+function updateRepos(config, remoteRepos) {
+  const localReposKeys = Object.keys(config.repos)
+  config.repos = remoteRepos
+  return Object.keys(remoteRepos).filter(r => !localReposKeys.includes(r.name))
+}
+
+function configPath() {
+  let path = process.argv.filter(x => x.startsWith('--path=')).map(x => x.replace('--path=', ''))
+  if (path.length == 0) {
+    return './.github_backup_config.json'
+  } else {
+    return path[0]
+  }
+}
+
+let config = await loadConfig(configPath());
+const remoteRepos = await fetchRepos(config.username, config.token)
+
+for (const name of updateRepos(config, remoteRepos)) {
+  const path = `./${name}`
+  if (fs.pathExistsSync(path)) {
+    const del = await question(`Delete ${path}? (y/n): `, {
+      choices: ['y', 'n']
+    })
+    if (del === 'y') {
+      await fs.remove(path)
+    }
+  }
+}
+
+for (const name of Object.keys(config.repos)) {
+  const path = `./${name}`
+  const repo = remoteRepos[name]
+  if (fs.pathExistsSync(path)) {
+    await $`cd ${path} && git pull`
+  } else {
+    const clone = await question(`Clone ${repo.ssh_url}? (y/n): `, {
+      choices: ['y', 'n']
+    })
+    if (clone === 'y') {
+      await $`git clone ${repo.ssh_url}`
+    }
+  }
+}
+
+await fs.writeFile(configPath, JSON.stringify(config, null, 2))
