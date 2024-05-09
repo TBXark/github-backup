d2136f885665232bbf984c4a0067aed9ba81b935
tbxark <tbxark@outlook.com>
2024-05-09 21:33:50 +0800

    chore: 安装脚本自动获取最新的release

--- init/install.sh
+++ init/install.sh
@@ -11,6 +11,12 @@ CURRENT_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 CURRENT_ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 BUILD_FROM_SOURCE="You can build from source: go install github.com/${AUTHOR}/${REPO_NAME}@latest"
 
+# If jq is installed, then fetch the latest release version
+if command -v jq &> /dev/null; then
+  VERSION=$(curl -s https://api.github.com/repos/${AUTHOR}/${REPO_NAME}/releases/latest | jq -r .tag_name)
+  echo "Latest release version: $VERSION"
+fi
+
 # if CURRENT_ARCH is x86_64 or amd64, then use x86
 if [ "$CURRENT_ARCH" == "x86_64" ] || [ "$CURRENT_ARCH" == "amd64" ]; then
   CURRENT_ARCH="x86"
