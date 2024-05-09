dcba9ba4065f1521e9748e89418799df7daaebee
tbxark <tbxark@outlook.com>
2024-05-09 18:26:50 +0800

    chore: 安装脚本对x86_64兼容

--- init/install.sh
+++ init/install.sh
@@ -7,11 +7,17 @@ VERSION=0.0.1
 BIN_NAME=github-backup
 CURRENT_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 CURRENT_ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+# if CURRENT_ARCH is x86_64 then use x86
+if [ "$CURRENT_ARCH" == "x86_64" ]; then
+  CURRENT_ARCH="x86"
+fi
+
 TARGET=${BIN_NAME}_${CURRENT_OS}_${CURRENT_ARCH}
 
 URL=https://github.com/TBXark/${BIN_NAME}/releases/download/${VERSION}/${TARGET}.tar.gz
 echo "Downloading ${BIN_NAME} from ${URL}"
-curl -L $URL | tar xz
-chmod +x ${TARGET}/github-backup
+curl -L $URL | tar -xz
+chmod +x ${TARGET}/${BIN_NAME}
 mv ${TARGET}/${BIN_NAME} /usr/local/bin/${BIN_NAME}
 rm -rf ${TARGET}
